26bfb63f69ad2abe3d7ac76d572ae1407a189216
DisabledMallis <mallisiscool@gmail.com>
2021-06-03 22:45:27 -0400

    IDK man cmake not like calling the shit

---
+++ Nuvola/main.cpp
@@ -0,0 +1,33 @@
+#include <Windows.h>
+#include <string>
+#include <iostream>
+#include <shlobj_core.h>
+
+#define EXPORT comment(linker, "/EXPORT:" __FUNCTION__ "=" __FUNCDNAME__)
+
+
+auto initialize() -> int {
+    AllocConsole();
+    FILE *fDummy;
+    freopen_s(&fDummy, "CONIN$", "r", stdin);
+    freopen_s(&fDummy, "CONOUT$", "w", stderr);
+    freopen_s(&fDummy, "CONOUT$", "w", stdout);
+    std::cout << "Loading original WinINet.dll..." << std::endl;
+
+    return 0;
+}
+
+extern "C" __declspec(dllexport) bool __stdcall DllMain(
+    HINSTANCE hinstDLL,  // handle to DLL module
+    DWORD fdwReason,     // reason for calling function
+    LPVOID lpReserved )  // reserved
+{
+    // Perform actions based on the reason for calling.
+    switch( fdwReason ) 
+    { 
+        case DLL_PROCESS_ATTACH:
+            initialize();
+            break;
+    }
+    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
